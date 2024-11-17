8c7efe7b5bd174491a3f3618198efd4b0a71daa3
Bikram Samantaray <samantaraybikram@gmail.com>
2024-11-17 10:01:38 +0530

    Codes to Create Git Repo

---
+++ Git_Repo/main.tf
@@ -0,0 +1,6 @@
+resource "github_repository" "Terra-Test-2" {
+  name = "Terra-Test-2"
+  description = "Lets Test"
+  visibility = "public"
+  auto_init = true
+}
---
+++ Git_Repo/output.tf
@@ -0,0 +1,8 @@
+output "RepoURL" {
+  value = github_repository.Terra-Test-2.html_url
+}
+
+
+# We can run this command to get the output value in terminal
+# $ terraform output RepoURL
+# "https://github.com/bikram255/Terra-Test-2"
